4f0e0a9241c9056cc8792dec245a54dee2d54264
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2024-05-12 17:13:13 +0900

    #2 プロジェクトを小さく表示し、詳細はクリックすることでダイアログとして見れるように変更

---
+++ src/components/ProjectDialog.vue
@@ -0,0 +1,122 @@
+<template>
+    <div id="black-back">
+        <div class="project-frame">
+            <div class="close-dialog" @click="closeDialog"></div>
+            <div class="project-content">
+                <div id="project-content-title">
+                    {{ project.title }}
+                </div>
+                <div class="project-image">
+                    <img
+                        v-if="project.imgPath"
+                        :src="getImgPath(project.imgPath)"
+                        height="300x"
+                        alt="イメージ画像"
+                    />
+                </div>
+                <div class="project-description">
+                    <ul>
+                        <li
+                            v-if="project.urlItems"
+                            v-for="urlItem in project.urlItems"
+                        >
+                            【参考URL】：<a
+                                v-bind:href="urlItem.url"
+                                target="_blank"
+                            >
+                                {{ urlItem.urlName }}
+                            </a>
+                        </li>
+                        <li>【概要】：{{ project.detail }}</li>
+                        <li>【利用技術】：{{ project.tech }}</li>
+                        <li>【成果】：{{ project.result }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { IProject } from "@/type/master";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+defineProps<{
+    project: IProject;
+    closeDialog: () => void;
+}>();
+
+const getImgPath = (imgPath: string) => {
+    return new URL(imgPath, import.meta.url).href;
+};
+</script>
+
+<style scoped>
+#black-back {
+    background-color: rgba(34, 34, 34, 0.25);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.close-dialog {
+    display: block;
+    position: relative;
+    width: 50px;
+    height: 50px;
+    margin: 10px 10px 0 auto;
+}
+.close-dialog:hover {
+    cursor: pointer;
+}
+.close-dialog::before, .close-dialog::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 50px;
+    height: 2px;
+    background-color: #000;
+    transform: translate(-50%, -50%);
+}
+.close-dialog::before {
+    transform: translate(-50%, -50%) rotate(45deg);
+}
+.close-dialog::after {
+    transform: translate(-50%, -50%) rotate(-45deg);
+}
+
+.project-frame {
+    width: 60%;
+    height: 90%;
+    border-radius: 20px;
+    background-color: #ffffff;
+    padding: 10px;
+    overflow: hidden;
+    z-index: 110;
+    box-shadow: 7px 7px 20px #222222;
+    overflow-y: scroll;
+}
+
+#project-content-title {
+    font-size: 54px;
+    margin: 10px auto;
+}
+
+.project-image {
+    max-width: 100%;
+    height: auto;
+    text-align: center;
+    object-fit: contain;
+}
+
+.project-description {
+    flex: 1;
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,40 +4,32 @@
             <h2>Projects</h2>
         </div>
         <div class="content-text" id="projects-text">
-            ※詳しくは<a href="https://github.com/nightshrine" target="_blank"
-                >Githubのマイページ</a
-            >をご覧ください。
-            <p class="pre-text">
-                ※業務で作成した成果物については、実際の画面などを載せることができません。予めご了承ください。
-            </p>
-            <div class="project-frame" v-for="project in projectList">
-                <div class="project-content">
-                    <div id="project-content-title">~{{ project.title }}~</div>
-                    <div class="project-image">
-                        <img
-                            v-if="project.imgPath"
-                            :src="getImgPath(project.imgPath)"
-                            height="300x"
-                            alt="イメージ画像"
-                        />
-                    </div>
-                    <div class="project-description">
-                        <ul>
-                            <li
-                                v-if="project.urlItems"
-                                v-for="urlItem in project.urlItems"
-                            >
-                                【参考URL】：<a
-                                    v-bind:href="urlItem.url"
-                                    target="_blank"
-                                >
-                                    {{ urlItem.urlName }}
-                                </a>
-                            </li>
-                            <li>【概要】：{{ project.detail }}</li>
-                            <li>【利用技術】：{{ project.tech }}</li>
-                            <li>【成果】：{{ project.result }}</li>
-                        </ul>
+            <div id="projects-detail">
+                ※詳しくは<a
+                    href="https://github.com/nightshrine"
+                    target="_blank"
+                    >Githubのマイページ</a
+                >をご覧ください。
+                <p class="pre-text">
+                    プロジェクトをクリックすることで、詳細をご覧いただけます。
+                </p>
+                <p class="pre-text">
+                    ※業務で作成した成果物については、実際の画面などを載せることができません。予めご了承ください。
+                </p>
+            </div>
+            <div class="project-list">
+                <div
+                    class="project-frame"
+                    @click="onClickProject(project)"
+                    v-for="project in projectList"
+                >
+                    <div class="project-content">
+                        <div id="project-content-title">
+                            {{ project.title }}
+                        </div>
+                        <div class="project-description">
+                            {{ project.detail }}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -46,6 +38,11 @@
             >をご覧ください。
         </div>
     </div>
+    <ProjectDialog
+        v-if="showPropjectDialog"
+        :project="showProject!"
+        :closeDialog="closeProjectDialog"
+    />
     <RouterView />
 </template>
 
@@ -53,38 +50,71 @@
 import { RouterView } from "vue-router";
 import Master from "@/master";
 import type { IProject } from "@/type/master";
+import ProjectDialog from "./ProjectDialog.vue";
+import { ref } from "vue";
 
 const projectList: IProject[] = Master.projectList;
 
-const getImgPath = (imgPath: string) => {
-    return new URL(imgPath, import.meta.url).href;
+// プロジェクト詳細ダイアログを表示するか
+const showPropjectDialog = ref(false);
+// 表示するプロジェクト
+const showProject = ref<IProject | undefined>();
+
+// プロジェクトをクリックしたときの処理
+const onClickProject = (project: IProject) => {
+    showProject.value = project;
+    showPropjectDialog.value = true;
 };
+
+// プロジェクト詳細ダイアログを閉じる
+const closeProjectDialog = () => {
+    showPropjectDialog.value = false;
+};
+
 </script>
 
 <style scoped>
-#project-content-title {
-    font-size: 56px;
-    margin: 10px auto;
-    text-align: center;
+#projects-detail {
+    margin-bottom: 50px;
+}
+
+.project-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
 }
 
 .project-frame {
+    max-width: 28%;
+    min-height: 200px;
     margin-bottom: 50px;
     box-shadow: 2px 3px 5px #222222;
     border-radius: 20px;
-    padding-top: 10px;
+    padding: 10px;
     overflow: hidden;
+    transition: box-shadow 0.5s;
+}
+.project-frame:hover {
+    cursor: pointer;
+    box-shadow: 7px 7px 20px #222222;
+    transition: box-shadow 0.5s;
+}
+
+#project-content-title {
+    font-size: 26px;
+    margin: 10px auto;
 }
 
 .project-image {
     max-width: 100%;
     height: auto;
     text-align: center;
-    object-fit: cover;
+    object-fit: contain;
 }
 
 .project-description {
     flex: 1;
-    padding: 0 20px;
+    opacity: 0.5;
+    font-size: 18px;
 }
 </style>
